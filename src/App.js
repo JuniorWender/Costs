debb3f5afb792b808a3923d62b61ba2c83ba58df
wender.junior <wender.junior@etaure.com.br>
2023-01-20 11:42:28 -0300

    create Navbar/footer and add style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import Home from './components/pages/Home'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/Contact">Contact</Link>
-        <Link to="/Company">Company</Link>
-        <Link to="/NewProject">New Project</Link>
-      </ul>
+      <Navbar/>
       <Container customClass="min-heigth">
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
           <Route path="/Contact" element={<Contact/>}/>
           <Route path="/Company" element={<Company/>}/>
           <Route path="/NewProject" element={<NewProject/>}/>
         </Routes>
       </Container>
+      <Footer/>
     </Router>
   );
 }
