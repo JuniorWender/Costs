d1ca9baa0223c6c2cb33dbc6677956da187e0b5b
wender.junior <wender.junior@etaure.com.br>
2023-01-20 10:45:18 -0300

    create basic components and add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/NewProject">New Project</Link>
+      </ul>
+      <Container customClass="min-heigth">
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/Contact" element={<Contact/>}/>
+          <Route path="/Company" element={<Company/>}/>
+          <Route path="/NewProject" element={<NewProject/>}/>
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
